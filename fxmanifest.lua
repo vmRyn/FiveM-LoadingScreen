bb9c5a350cf21f4684ec8bb0e2142debe6437aa0
Ryn <31045220+vmRyn@users.noreply.github.com>
2021-07-01 10:38:16 +0100

    Upload

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Ryn'
+description 'a FiveM Loading Screen'
+version '1.0.0'
+
+files {
+    'index.html',
+    'css/stylesheet.css',
+    'css/config.css',
+    'js/config.js',
+    'js/app.js',
+    'js/music.js',
+    'js/fullscreen-video.js',
+    'js/snowflake-effect.js',
+    'images/bg1.png', 
+    'images/bg2.png', 
+    'images/bg3.png', 
+    'images/bg4.png', 
+    'images/bg5.png',
+    'images/bg6.png', 
+    'images/logo.png',
+    'audios/audio.mp3',
+    'videos/video.mp4'
+}
+
+loadscreen 'index.html'
---
+++ js/config.js
@@ -0,0 +1,9 @@
+// <!-- © Ryn#2512 | Need Help Message me on Discord -->
+
+var config = {
+    "text": {
+        "mainlogo": "./images/logo.png",
+        "servertitle": "SERVER TITLE",
+        "songname": "Indiana - Mess Around (Etherwood Remix)",
+    }
+}
